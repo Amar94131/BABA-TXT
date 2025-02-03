76ceec26447dbcc860883c30bd4a7b2c4ee4e723
Amar94131 <jaysingh85029@gmail.com>
2025-02-04 01:42:38 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -6,7 +6,7 @@ API_ID    = os.environ.get("API_ID", "26513107")
 API_HASH  = os.environ.get("API_HASH", "f14ce4b58dc8812cfc9665588472f2d4")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7601649831:AAEMQ9chNVKZe2hm4wEHN4nmgBd8vqeOvKI") 
 
-AUTH_CHANNELS = os.environ.get("AUTH_CHANNEL", "-1002330754414, -1002377421372")  # ✅ os.environ का सही उपयोग करें
+AUTH_CHANNELS = os.environ.get("AUTH_CHANNEL", "-1002156040011, -1002377421372")  # ✅ os.environ का सही उपयोग करें
 AUTH_CHANNELS = [int(channel_id) for channel_id in AUTH_CHANNELS.split(",")]
 
 #WEBHOOK = True  # Don't change this
